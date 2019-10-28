235352abb9aa1270448e825adc1eb1cc5307990e
Nico Coetzee <nicc777@gmail.com>
2019-10-28 12:00:17 +0200

    Done with file renaming part. Directory renaming next - Issue #6

--- py_workdocs_prep/py_workdocs_prep.py
+++ py_workdocs_prep/py_workdocs_prep.py
@@ -2,6 +2,8 @@ import os
 import re
 import shutil
 
+VALID_NAME_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890-_.'
+
 directories_to_delete_if_found = [
     '.git',
     'venv*',
@@ -23,6 +25,7 @@ data['all_original_dirs_only'] = list()
 data['processing'] = dict()
 data['processing']['directories_deleted'] = list()
 data['processing']['files_deleted'] = list()
+data['processing']['renamed_files'] = list()
 
 
 def is_directory_to_be_deleted(current_directory_name: str, directories_to_delete_if_found: list=directories_to_delete_if_found)->bool:
@@ -46,14 +49,14 @@ def is_directory_to_be_deleted(current_directory_name: str, directories_to_delet
 
 
 def is_file_starting_or_ending_with_tilde(current_file_with_full_path: str)->bool:
-    file = current_file_with_full_path.split(os.sep)[-1]
-    last_part = current_file_with_full_path.split(os.sep)[-1]
+    file_name = current_file_with_full_path.split(os.sep)[-1]
+    #last_part = current_file_with_full_path.split(os.sep)[-1]  # Refactored furing issue #6 - can be deleted (duplicate of file_name)
     must_delete = False
-    if re.search('^~', file) is not None or re.search('~$', file) is not None:
+    if re.search('^~', file_name) is not None or re.search('~$', file_name) is not None:
         must_delete = True
     if re.search('.tmp$', current_file_with_full_path):
         must_delete = True
-    if re.search('^\\.', last_part) is not None or re.search('\\.$', last_part):
+    if re.search('^\\.', file_name) is not None or re.search('\\.$', file_name):
         must_delete = True
     if must_delete is True:
         try:
@@ -63,6 +66,37 @@ def is_file_starting_or_ending_with_tilde(current_file_with_full_path: str)->boo
             warnings.append('Error while deleting file "{}"'.format(current_file_with_full_path))
         return True
     return False
+
+
+def file_rename(current_file_with_full_path: str)->str:
+    file_name = current_file_with_full_path.split(os.sep)[-1]
+    path_name = ''
+    if os.sep == '/':
+        path_name = '/'.join(current_file_with_full_path.split(os.sep)[:-1]) 
+    else:
+        path_name = '\\'.join(current_file_with_full_path.split(os.sep)[:-1]) 
+    final_file_name = ''
+    for char in file_name:
+        if char not in VALID_NAME_CHARS:
+            final_file_name = '{}{}'.format(final_file_name, '_')
+        else:
+            final_file_name = '{}{}'.format(final_file_name, char)
+    target_file = '{}{}{}'.format(path_name, os.sep, final_file_name)
+    if file_name != final_file_name:
+        pattern = re.compile('__*')
+        final_file_name = pattern.sub('_', final_file_name)
+        target_file = '{}{}{}'.format(path_name, os.sep, final_file_name)
+        try:
+            shutil.move(current_file_with_full_path, target_file)
+            data['processing']['renamed_files'].append(
+                (
+                    current_file_with_full_path,
+                    target_file,
+                )
+            )
+        except:
+            warnings.append('Error while moving file "{}"'.format(current_file_with_full_path))
+    return target_file
 
 
 def recurse_dir(root_dir, directories_to_delete_if_found: list=directories_to_delete_if_found):
@@ -84,7 +118,10 @@ def recurse_dir(root_dir, directories_to_delete_if_found: list=directories_to_de
                 if is_file_starting_or_ending_with_tilde(current_file_with_full_path=item_full_path) is False:
                     keep_file += 1
                 if keep_file > 0:
-                    data['all_original_files'].append(item_full_path)
+                    final_file_name_and_full_path = file_rename(current_file_with_full_path=item_full_path)
+                    data['all_original_files'].append(final_file_name_and_full_path)
+                else:
+                    warnings.append('File "{}" was marked to be deleted'.format(item_full_path))
 
 
 def dump_warnings():
